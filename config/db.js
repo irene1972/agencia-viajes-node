44ee3e82b8f2cf9223c8fcafdb869d5e209ac39f
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-15 00:55:09 +0200

    preparar deployment

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 
 dotenv.config();
-//console.log(process.env.DATABASE);
-
+//const db=new Sequelize(process.env.DATABASE_URL,{
 const db=new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host:process.env.DB_HOST,
     port:3306,
