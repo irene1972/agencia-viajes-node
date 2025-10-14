734c2615ed1731ef3c6cfbff48b4d303d302db14
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-14 17:44:27 +0200

    conectar a una bd mysql

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //const express=require('express');
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 
 const app=express();
+
+//conectar la base de datos
+db.authenticate()
+    .then(()=>console.log('Base de datos conectada...'))
+    .catch(error=>console.log(error));
 
 //definir puerto
 const port=process.env.PORT || 4000;
