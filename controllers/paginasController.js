0514a20e427d6aa1ee2226c8c211d9b971729789
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-14 23:53:16 +0200

    realizar una consulta múltiple con promises

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -3,14 +3,20 @@ import {Testimonial} from '../models/Testimoniales.js';
 
 const paginaInicio=async (req,res)=>{
 
+    const promiseDB=[];
+
+    promiseDB.push(Viaje.findAll({limit:3}));
+    promiseDB.push(Testimonial.findAll({limit:3}));
+
     try {
         //consultar 3 viajes del modelo Viaje
-        const viajes=await Viaje.findAll({limit:3});
+        const resultado=await Promise.all(promiseDB);
 
         res.render('inicio',{
             pagina:'Inicio',
             clase:'home',
-            viajes
+            viajes:resultado[0],
+            testimoniales:resultado[1]
         });
     } catch (error) {
         console.log(error);
